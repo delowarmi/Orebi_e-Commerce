eddedbe51368f5e55d20bebab599834d1f4b4f3a
Muhammad Delowar Hosen MI <md48735002@gmail.com>
2024-12-30 10:49:56 +0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,15 @@ const Header = () => {
   return (
     <div className='bg-subnavColor py-5  '>
       <Container>
-      <Flex className={' sm:gap-x-0 md:gap-x-1 lg:gap-x-2 xl:gap-x-3'}>
-        <div className="pt-2 sm:w-[12%] md:w-[15%] lg:w-[18%] xl:w-[20%]">
+      <Flex className={' mobile:gap-x-0 android:gap-x-1 taplet:gap-x-2 laptop:gap-x-3'}>
+        <div className="pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
         <Category/>
         </div>
-        <div className=" relative sm:w-[300px] md:w-[550px] lg:w-[700px] xl:w-[900px]">
+        <div className=" relative mobile:w-[300px] android:w-[550px] taplet:w-[700px] laptop:w-[1200px]">
           <input type="text"className='className = p-3 bg-white rounded-lg w-full border border-InfoColor'placeholder='Search Products' />
           <FaSearch className='absolute right-4 top-1/2 -translate-y-1/2'/>
         </div>
-        <div className=" pt-2 sm:w-[12%] md:w-[15%] lg:w-[18%] xl:w-[20%]">
+        <div className=" pt-2 mobile:w-[12%] android:w-[15%] taplet:w-[18%] laptop:w-[20%]">
         <Flex className={'justify-center'}>
                 <User/>
                 <FaSortDown className='ml-1'/>
--- src/parts/Banner.jsx
+++ src/parts/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='bg-bannerImg py-[270px] bg-no-repeat bg-center bg-cover xl:bg-bannerImg xl:py-[270px] xl:bg-no-repeat xl:bg-center xl:bg-cover lg:bg-bannerImg lg:py-[230px] lg:bg-no-repeat lg:bg-center lg:bg-cover md:bg-bannerImg md:py-[190px] md:bg-no-repeat md:bg-center md:bg-cover sm:bg-bannerImg sm:py-[150px] sm:bg-no-repeat sm:bg-center sm:bg-cover'>  
+    <div className='bg-bannerImg py-[270px] bg-no-repeat bg-center bg-cover laptop:bg-bannerImg laptop:py-[270px] laptop:bg-no-repeat laptop:bg-center laptop:bg-cover taplet:bg-bannerImg taplet:py-[230px] taplet:bg-no-repeat taplet:bg-center taplet:bg-cover android:bg-bannerImg android:py-[190px] android:bg-no-repeat android:bg-center android:bg-cover mobile:bg-bannerImg mobile:py-[150px] mobile:bg-no-repeat mobile:bg-center mobile:bg-cover'>  
     </div>
   )
 }
